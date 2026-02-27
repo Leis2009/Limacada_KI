a71f6b0c70d3a1e329f2e1cac646fbd220a2c69a
Leis2009 <112397352+Leis2009@users.noreply.github.com>
2026-02-27 15:02:16 +0100

    Add files via upload
    tokinizer hinzugefügt bis kapitel 2.5

--- Limacada_KI/main.py
+++ Limacada_KI/main.py
@@ -1,6 +1,47 @@
 import urllib.request
 import re
 
+"""
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)',
+        text)
+        preprocessed = [
+        item.strip() for item in preprocessed if
+        item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+        """
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)',
+        text)
+        preprocessed = [
+        item.strip() for item in preprocessed if
+        item.strip()
+        ]
+        preprocessed = [item if item in self.str_to_int
+        else "<|unk|>" for item in
+        preprocessed]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+
 def main():
     url = ("https://raw.githubusercontent.com/rasbt/"
            "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
@@ -20,7 +61,47 @@ def main():
     preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
     preprocessed = [item.strip() for item in preprocessed if
                     item.strip()]
-    print(len(preprocessed))
+    #print(len(preprocessed))
+    #print(preprocessed[:30])
+
+    all_words = sorted(set(preprocessed))
+    vocab_size = len(all_words)
+    #print(vocab_size)
+
+    vocab = {token: integer for integer, token in
+             enumerate(all_words)}
+
+    #for i, item in enumerate(vocab.items()):
+     #   print(item)
+      #  if i >= 100:
+       #     break
+
+    #tokenizer = SimpleTokenizerV1(vocab)
+    #text = """"It's the last he painted, you know,"
+    #Mrs. Gisburn said with pardonable pride."""""
+    #ids = tokenizer.encode(text)
+    #print(ids)
+    #print(tokenizer.decode(ids))
+
+    all_tokens = sorted(list(set(preprocessed)))
+    all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+    vocab = {token: integer for integer, token in
+             enumerate(all_tokens)}
+    #print(len(vocab.items()))
+
+    #for i, item in enumerate(list(vocab.items())[-5:]):
+     #   print(item)
+
+
+    text1 = "Hello, do you like tea?"
+    text2 = "In the sunlit terraces of the palace."
+    text = " <|endoftext|> ".join((text1, text2))
+    #print(text)
+
+    tokenizer = SimpleTokenizerV2(vocab)
+    #print(tokenizer.encode(text))
+
+    print(tokenizer.decode(tokenizer.encode(text)))
 
 if __name__ == '__main__':
     main()
